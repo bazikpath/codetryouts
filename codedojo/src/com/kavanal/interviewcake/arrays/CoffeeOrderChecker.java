2cdf6c1854b07b4ff6de00085e012b1f0c8467a9
Sunil Jacob <support@urduathome.com>
2020-04-15 15:32:15 -0700

    Adding directory of project

---
+++ codedojo/src/com/kavanal/interviewcake/arrays/CoffeeOrderChecker.java
@@ -0,0 +1,109 @@
+package com.kavanal.interviewcake.arrays;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class CoffeeOrderChecker {
+
+    public static boolean isFirstComeFirstServed(int[] takeOutOrders, int[] dineInOrders, int[] servedOrders) {
+
+        int takeOutIndex = 0;
+        int dineInIndex = 0;
+        // check if we're serving orders first-come, first-served
+        for (int order : servedOrders) {
+            if (takeOutIndex < takeOutOrders.length && order == takeOutOrders[takeOutIndex]) {
+                takeOutIndex++;
+            } else if (dineInIndex < dineInOrders.length && order == dineInOrders[dineInIndex]) {
+                dineInIndex++;
+            } else {
+                return false;
+            }
+        }
+
+        if (takeOutIndex != takeOutOrders.length || dineInIndex != dineInOrders.length)
+            return false;
+
+        return true;
+    }
+
+
+    // tests
+
+    @Test
+    public void bothRegistersHaveSameNumberOfOrdersTest() {
+        final int[] takeOutOrders = {1, 4, 5};
+        final int[] dineInOrders = {2, 3, 6};
+        final int[] servedOrders = {1, 2, 3, 4, 5, 6};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertTrue(result);
+    }
+
+    @Test
+    public void registersHaveDifferentLengthsTest() {
+        final int[] takeOutOrders = {1, 5};
+        final int[] dineInOrders = {2, 3, 6};
+        final int[] servedOrders = {1, 2, 6, 3, 5};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertFalse(result);
+    }
+
+    @Test
+    public void oneRegisterIsEmptyTest() {
+        final int[] takeOutOrders = {};
+        final int[] dineInOrders = {2, 3, 6};
+        final int[] servedOrders = {2, 3, 6};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertTrue(result);
+    }
+
+    @Test
+    public void servedOrdersIsMissingOrdersTest() {
+        final int[] takeOutOrders = {1, 5};
+        final int[] dineInOrders = {2, 3, 6};
+        final int[] servedOrders = {1, 6, 3, 5};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertFalse(result);
+    }
+
+    @Test
+    public void servedOrdersHasExtraOrders() {
+        final int[] takeOutOrders = {1, 5};
+        final int[] dineInOrders = {2, 3, 6};
+        final int[] servedOrders = {1, 2, 3, 5, 6, 8};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertFalse(result);
+    }
+
+    @Test
+    public void oneRegisterHasExtraOrders() {
+        final int[] takeOutOrders = {1, 9};
+        final int[] dineInOrders = {7, 8};
+        final int[] servedOrders = {1, 7, 8};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertFalse(result);
+    }
+
+
+    @Test
+    public void oneRegisterHasUnservedOrders() {
+        final int[] takeOutOrders = {55, 9};
+        final int[] dineInOrders = {7, 8};
+        final int[] servedOrders = {1, 7, 8, 9};
+        final boolean result = isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        assertFalse(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(CoffeeOrderChecker.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
---
+++ codedojo/src/com/kavanal/interviewcake/arrays/MergeMeetings.java
@@ -0,0 +1,189 @@
+package com.kavanal.interviewcake.arrays;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class MergeMeetings {
+
+    public static class Meeting {
+
+        private int startTime;
+        private int endTime;
+
+        public Meeting(int startTime, int endTime) {
+            // number of 30 min blocks past 9:00 am
+            this.startTime = startTime;
+            this.endTime   = endTime;
+        }
+
+        public int getStartTime() {
+            return startTime;
+        }
+
+        public void setStartTime(int startTime) {
+            this.startTime = startTime;
+        }
+
+        public int getEndTime() {
+            return endTime;
+        }
+
+        public void setEndTime(int endTime) {
+            this.endTime = endTime;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (!(o instanceof Meeting)) {
+                return false;
+            }
+            final Meeting meeting = (Meeting) o;
+            return startTime == meeting.startTime && endTime == meeting.endTime;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = result * 31 + startTime;
+            result = result * 31 + endTime;
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", startTime, endTime);
+        }
+    }
+
+    public static List<Meeting> mergeRanges(List<Meeting> meetings) {
+
+        List<Meeting> mergedMeetings = new ArrayList<>();
+        //Base Case and edge cases
+
+        // merge meetings ranges
+        ArrayList<Meeting> sortedMeetings = new ArrayList<>(meetings.size());
+        sortedMeetings.addAll(meetings);
+
+        sortedMeetings.sort(new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting o1, Meeting o2) {
+                return o1.getStartTime() - o2.getStartTime();
+            }
+        });
+
+        mergedMeetings.add(sortedMeetings.get(0));
+        for (Meeting currentMeeting : sortedMeetings) {
+            Meeting lastMergedMeeting = mergedMeetings.get(mergedMeetings.size() - 1);
+
+            if (currentMeeting.getEndTime()<= lastMergedMeeting.getStartTime()) {
+                currentMeeting.setEndTime(lastMergedMeeting.getEndTime());
+            } else {
+                mergedMeetings.add(currentMeeting);
+            }
+        }
+
+        return mergedMeetings;
+    }
+
+
+    // tests
+
+    @Test
+    public void meetingsOverlapTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(1, 3), new Meeting(2, 4));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(1, 4));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void  meetingsTouchTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(5, 6), new Meeting(6, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(5, 8));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingContainsOtherMeetingTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(1, 8), new Meeting(2, 5));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(1, 8));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingsStaySeparateTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(1, 3), new Meeting(4, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(1, 3), new Meeting(4, 8)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void multipleMergedMeetingsTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(1, 4), new Meeting(2, 5), new Meeting (5, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(1, 8));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingsNotSortedTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(5, 8), new Meeting(1, 4), new Meeting(6, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(1, 4), new Meeting(5, 8)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneLongMeetingContainsSmallerMeetingsTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(1, 10), new Meeting(2, 5), new Meeting(6, 8),
+                new Meeting(9, 10), new Meeting(10, 12)
+        );
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(1, 12)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sampleInputTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(0, 1), new Meeting(3, 5), new Meeting(4, 8),
+                new Meeting(10, 12), new Meeting(9, 10)
+        );
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(0, 1), new Meeting(3, 8), new Meeting(9, 12)
+        );
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(MergeMeetings.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
